e3aaed4a03d79be411c060d4544f1a0d75c4c5d3
Sastha K L <sastha@solutionmatrix.com.sg>
2013-09-19 10:53:28 +0800

    remove commit errors

--- server/Services.php
+++ server/Services.php
@@ -6,11 +6,7 @@
 	//JSON cannot work with regular $_POST and $_GET
 	$data = file_get_contents("php://input");
 	$receivedData = json_decode($data);
-<<<<<<< HEAD
 	
-=======
-	print_r($_SERVER);
->>>>>>> 8d55cfedbf80318f7c6799501a4f9ead88371f56
 	$id = explode("/", $_SERVER['REQUEST_URI']);
 	print_r($_SERVER['REQUEST_METHOD']);
 	print_r($id);
@@ -54,78 +50,7 @@
 		        }
 		        
 		    break;
-<<<<<<< HEAD
 		    
-=======
-		    case 'registeriOSDevice':
-		    	if(isset($receivedData->{"deviceToken"}) && isset($receivedData->{"clientID"})){
-		        	$deviceToken = $receivedData->{"deviceToken"};
-		        	$clientID   = $receivedData->{"clientID"};
-		        	$IPAddress  = $receivedData->{"ipAddress"};
-
-		        	$response = $push_notification->registeriOSDevice($deviceToken,$clientID, $IPAddress);
-		        }
-		        else{
-		        	$response = array("status" => 0,
-	                      "message"=> "All fields needs to be set");
-		        	echo json_encode($response);
-		        }
-		    break;
-		    case 'registerProfessional':
-		    	if(isset($receivedData->{"professionalDetails"}) && isset($receivedData->{"clientID"})){
-
-		        	$professionalDetails = $receivedData->{"professionalDetails"};
-		        	$clientID   = $receivedData->{"clientID"};
-
-		        	$response = $professional->registerProfessional($professionalDetails,$clientID);
-		        }
-		        else{
-		        	$response = array("status" => 0,
-	                      "message"=> "All fields needs to be set");
-		        	echo json_encode($response);
-		        }
-		    break;
-		    case 'registerLocation':
-		    	if(isset($receivedData->{"locationDetails"}) && isset($receivedData->{"clientID"})){
-
-		        	$locationDetails = $receivedData->{"locationDetails"};
-		        	$clientID   = $receivedData->{"clientID"};
-
-		        	$response = $location->registerLocation($locationDetails,$clientID);
-		        }
-		        else{
-		        	$response = array("status" => 0,
-	                      "message"=> "All fields needs to be set");
-		        	echo json_encode($response);
-		        }
-		    break;
-		    case 'checkStatus':
-		    	if(isset($receivedData->{"clientID"})){
-
-		        	$clientID   = $receivedData->{"clientID"};
-
-		        	$response = $system->checkStatus($clientID);
-		        }
-		        else{
-		        	$response = array("status" => 0,
-	                      "message"=> "All fields needs to be set");
-		        	echo json_encode($response);
-		        }
-		    break;
-		    case 'getUserList':
-		    	if(isset($receivedData->{"clientID"})){
-
-		        	$clientID   = $receivedData->{"clientID"};
-
-		        	$response = $user->getUserList($clientID);
-		        }
-		        else{
-		        	$response = array("status" => 0,
-	                      "message"=> "All fields needs to be set");
-		        	echo json_encode($response);
-		        }
-		    break;
->>>>>>> 8d55cfedbf80318f7c6799501a4f9ead88371f56
 		}
 	}
 	else {
